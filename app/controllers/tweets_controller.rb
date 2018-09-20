dcded497fde318b90d6d35b8afc802e80a456ccf
TomoyoshiKawamura <tomo.inf.universe@gmail.com>
2018-09-20 14:31:28 +0900

    投稿機能実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,7 +1,20 @@
 class TweetsController < ApplicationController
+
   def index
-    @tweets = Tweet.all
+    @tweets = Tweet.all.order('created_at desc')
   end
+
   def new
   end
+
+  def create
+    Tweet.create(tweet_params)
+  end
+
+  private
+  def tweet_params
+    params.permit(:name,:image,:text)
+  end
+
+
 end
