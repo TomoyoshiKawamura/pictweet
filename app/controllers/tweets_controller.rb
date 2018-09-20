a38243edd9c643f5292d66b6c0f0f55c62c82dd1
TomoyoshiKawamura <tomo.inf.universe@gmail.com>
2018-09-20 16:52:20 +0900

    app/views/devise/registrations/new.html.erbを以下のように編集しましょう

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,5 @@
 class TweetsController < ApplicationController
+  before_action :move_to_index,except: :index
 
   def index
     @tweets = Tweet.page(params[:page]).per(5).all.order('created_at desc')
@@ -9,6 +10,10 @@ class TweetsController < ApplicationController
 
   def create
     Tweet.create(tweet_params)
+  end
+
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
   end
 
   private
