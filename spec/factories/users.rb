09f28effcf527c51cfcaba3cef93d3c8ab1778e5
TomoyoshiKawamura <tomo.inf.universe@gmail.com>
2018-10-04 12:09:14 +0900

    Save

---
+++ spec/factories/users.rb
@@ -0,0 +1,11 @@
+FactoryGirl.define do
+
+  factory :user do
+    nickname            "abe"
+    email               "kkk@gmail.com"
+    password            "llllllll"
+    password_confirmation "llllllll"
+    id 1
+  end
+
+end
